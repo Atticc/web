fb49b4fe9d9e23f5d09605e96967de65198ad54f
cylim <cylim226@gmail.com>
2022-07-10 21:08:51 +0800

    add app name env varibale

--- apps/web/src/app/config.ts
+++ apps/web/src/app/config.ts
@@ -4,3 +4,4 @@ export const GALAXY_OAT_ENDPOINT: string =
   process.env.NEXT_PUBLIC_GALAZY_OAT_ENDPOINT || 'https://graphigo.stg.galaxy.eco/query'
 export const POAP_API_URL: string = process.env.NEXT_PUBLIC_POAP_API_URL || 'https://frontend.poap.tech'
 export const ALCHEMY_RPC_ETH: string = process.env.NEXT_PUBLIC_ALCHEMY_RPC_ETH || ''
+export const APP_NAME: string = process.env.NEXT_PUBLIC_APP_NAME || 'Atticc'
