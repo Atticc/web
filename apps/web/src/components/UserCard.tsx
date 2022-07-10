8c2b659a9cf3de7c054ee416342798d10c715bb6
cylim <cylim226@gmail.com>
2022-07-10 20:57:34 +0800

    add following modal

--- apps/web/src/components/UserCard.tsx
+++ apps/web/src/components/UserCard.tsx
@@ -7,11 +7,13 @@ import { useFollowStatus } from "../graphql/cyberconnect/queries/getFollowStatus
 import { formatAddress } from "../utils/helper";
 import { useWeb3 } from "../utils/Web3Context";
 import { PrimaryDarkButton } from "./buttons/Buttons";
+import UserConnectionModal from "./modal/UserConnectionModal";
 
 export const UserCard = ({ user, isDetail = false }: { user: IUser, isDetail?: boolean }) => {
+  const [showConnection, setShowConnection] = useState(false)
   const [isFollowing, setIsFollowing] = useState(false);
   const [loading, setLoading] = useState(false);
-  const {address, cyberConnect} = useWeb3()
+  const { address, cyberConnect } = useWeb3()
 
   const handleFetchFollowStatusResult = (data: any) => {
     setIsFollowing(data?.isFollowing)
@@ -29,11 +31,11 @@ export const UserCard = ({ user, isDetail = false }: { user: IUser, isDetail?: b
   const onFollow = async () => {
     try {
       setLoading(true)
-      isFollowing 
+      isFollowing
         ? await cyberConnect?.disconnect(user?.address, '')
         : await cyberConnect?.connect(user?.address, '', ConnectionType.FOLLOW)
       await refetchFollowStatus()
-    } catch(err: any) {
+    } catch (err: any) {
       console.warn(err.message)
     } finally {
       setLoading(false)
@@ -41,7 +43,7 @@ export const UserCard = ({ user, isDetail = false }: { user: IUser, isDetail?: b
   }
 
   useEffect(() => {
-    if(address && user.address) {
+    if (address && user.address) {
       refetchFollowStatus()
     }
   }, [user.address, address])
@@ -61,13 +63,18 @@ export const UserCard = ({ user, isDetail = false }: { user: IUser, isDetail?: b
               </Typography>
             </Stack>
           </Stack>
-          <Stack direction={'row'} spacing={3}>
+          <Stack direction={'row'} spacing={3} onClick={() => setShowConnection(true)} sx={{
+            cursor: 'pointer',
+            ':hover': {
+              filter: 'opacity(0.8)'
+            }
+          }}>
             <Stack direction={'column'} alignItems={'center'}>
               <Typography variant='body1'>
                 Follower
               </Typography>
               <Typography variant='h6'>
-               {user?.followerCount}
+                {user?.followerCount || 0}
               </Typography>
             </Stack>
             <Stack direction={'column'} alignItems={'center'}>
@@ -75,32 +82,65 @@ export const UserCard = ({ user, isDetail = false }: { user: IUser, isDetail?: b
                 Following
               </Typography>
               <Typography variant='h6'>
-               {user?.followingCount}
+                {user?.followingCount || 0}
               </Typography>
             </Stack>
           </Stack>
           {address && address !== user?.address ? <PrimaryDarkButton
             textcontent={loading ? 'loading...' : isFollowing ? 'Followed' : 'Follow'}
             onClick={onFollow}
-          /> : null }
-         
+          /> : null}
+
         </Stack>
       </Grid>
+      <UserConnectionModal address={user.address} open={showConnection} onClose={() => setShowConnection(false)} />
     </Grid>
   }
 
-  return <Link href={`/users/${user.address}`}>
-    <Grid item>
-      <Grid container direction={'column'}>
-        <Grid item sx={{ borderRadius: 3, background: colorTheme.backgroundDark500.main, paddingX: 2, paddingY: 3, marginX: 2, marginY: 3, cursor: 'pointer' }}>
-          <Typography variant="body1">
-            {user.address}
+  return <Grid item sx={{ borderRadius: 3, border: 0.5, paddingX: 2, paddingY: 3, cursor: 'pointer', height: 200, width: 320 }}>
+       <Link href={`/users/${user.address}`}>
+      <Stack direction={'column'} alignItems={'center'}>
+        <Stack direction={'row'} alignItems={'center'} paddingBottom={2}>
+          <Avatar variant='circular' src={user?.avatar || ''} />
+          <Stack direction={'column'} paddingLeft={1}>
+            <Typography variant='h5'>
+              {user?.domain || null}
+            </Typography>
+            <Typography variant='body1'>
+              {formatAddress(user?.address)}
+            </Typography>
+          </Stack>
+        </Stack>
+        <Stack direction={'row'} spacing={3} onClick={() => setShowConnection(true)} sx={{
+          cursor: 'pointer',
+          ':hover': {
+            filter: 'opacity(0.8)'
+          }
+        }}>
+          <Stack direction={'column'} alignItems={'center'}>
+            <Typography variant='body1'>
+              Follower
+            </Typography>
+            <Typography variant='h6'>
+              {user?.followerCount || 0}
+            </Typography>
+          </Stack>
+          <Stack direction={'column'} alignItems={'center'}>
+            <Typography variant='body1'>
+              Following
+            </Typography>
+            <Typography variant='h6'>
+              {user?.followingCount || 0}
+            </Typography>
+          </Stack>
+        </Stack>
+        <Stack>
+          <Typography variant='body2'>
+            {user?.recommendationReason}
           </Typography>
-          <Typography variant="body1">
-            {user.domain}
-          </Typography>
-        </Grid>
-      </Grid>
-    </Grid>
+
+        </Stack>
+      </Stack>
   </Link>
+    </Grid>
 }
--- apps/web/src/graphql/cyberconnect/queries/getConnections.tsx
+++ apps/web/src/graphql/cyberconnect/queries/getConnections.tsx
@@ -2,73 +2,88 @@ import { gql, request } from "graphql-request";
 import {useQuery} from 'react-query';
 import { CYBERCONNECT_ENDPOINT } from "../../../app/config";
 
+export interface GetConnectionRequest {
+    type?: string;
+    address: string;
+    first: number;
+    after?: string;
+    namespace?: string;
+}
+
 export const GET_ADDR_CONNECTION_QUERY = gql`
-    query identity(
+    query GetConnection(
         $address: String!
         $first: Int
         $after: String
         $namespace: String
+        $type: ConnectionType
     ) {
         identity(address: $address, network: ETH) {
-            avatar
             followings(
-                namespace: $namespace
-                type: FOLLOW
+                type: $type
                 first: $first
                 after: $after
+                namespace: $namespace
             ) {
                 pageInfo {
                     hasNextPage
+                    endCursor
                 }
                 list {
                     address
                     avatar
-                    ens
+                    domain
                 }
             }
             followers(
-                namespace: $namespace
-                type: FOLLOW
+                type: $type
                 first: $first
                 after: $after
+                namespace: $namespace
             ) {
                 pageInfo {
                     hasNextPage
+                    endCursor
                 }
                 list {
                     address
                     avatar
-                    ens
+                    domain
                 }
             }
             friends(
-                namespace: $namespace
-                type: FOLLOW
+                type: $type
                 first: $first
                 after: $after
+                namespace: $namespace
             ) {
                 pageInfo {
                     hasNextPage
+                    endCursor
                 }
                 list {
                     address
                     avatar
-                    ens
+                    domain
                 }
             }
         }
     }
 `;
 
-
-export function useConnections() {
-    return useQuery("connections", async () => {
-        const {
-            identity: { data },
-        } = await request(
+export const getConnection = async ({ address, first, after }: GetConnectionRequest) => {
+    try {
+        const { identity } = await request(
             CYBERCONNECT_ENDPOINT,
-            GET_ADDR_CONNECTION_QUERY
+            GET_ADDR_CONNECTION_QUERY, 
+            { address, first, after }
         );
-        return data;
-    });
+        return identity;
+    } catch (err) {
+        return null;
+    }
+}
+
+export function useConnections({ address, first, after, ...props }: GetConnectionRequest) {
+    return useQuery(["connections", address], async () => getConnection({address, first, after}), { enabled: false, ...props });
 }
